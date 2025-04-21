2ed6559cd6d97a67ab88824ef7e5d4ce2818619f
Leonardo Nunes <leonunesbs@gmail.com>
2025-04-21 18:45:06 -0300

    fix: fix

--- src/app/(withAuth)/playlists/[id]/[playlistItemId]/page.tsx
+++ src/app/(withAuth)/playlists/[id]/[playlistItemId]/page.tsx
@@ -6,20 +6,21 @@ import { redirect } from "next/navigation";
 export default async function PlaylistItemPage({
   params,
 }: {
-  params: { id: string; playlistItemId: string };
+  params: Promise<{ id: string; playlistItemId: string }>;
 }) {
-  const playlist = await api.playlist.getById({ id: params.id });
+  const { id, playlistItemId } = await params;
+  const playlist = await api.playlist.getById({ id });
 
   if (!playlist) {
     redirect("/playlists");
   }
 
   const currentIndex = playlist.items.findIndex(
-    (item) => item.id === params.playlistItemId,
+    (item) => item.id === playlistItemId,
   );
 
   if (currentIndex === -1) {
-    redirect(`/playlists/${params.id}`);
+    redirect(`/playlists/${id}`);
   }
 
   const currentItem = playlist.items[currentIndex];
