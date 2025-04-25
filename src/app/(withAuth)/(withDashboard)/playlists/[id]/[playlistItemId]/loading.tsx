958600d3700840161dc906d6d95486d8e0948a74
Leonardo Nunes <leonunesbs@gmail.com>
2025-04-25 19:45:06 -0300

    fix: fix UI

--- src/app/(withAuth)/(withDashboard)/playlists/[id]/[playlistItemId]/loading.tsx
+++ src/app/(withAuth)/(withDashboard)/playlists/[id]/[playlistItemId]/loading.tsx
@@ -25,7 +25,7 @@ export default function PlaylistItemLoading() {
           <CardHeader>
             <div className="flex items-start justify-between">
               <div>
-                <CardTitle className="text-lg">
+                <CardTitle>
                   <Skeleton className="h-6 w-48" />
                 </CardTitle>
                 <CardDescription>
@@ -36,21 +36,10 @@ export default function PlaylistItemLoading() {
             </div>
           </CardHeader>
           <CardContent className="space-y-4">
-            <div className="prose mb-4">
-              <Skeleton className="h-32 w-full" />
-            </div>
-            <div className="flex justify-center">
-              <Skeleton className="h-[300px] w-[400px] rounded" />
-            </div>
+            <Skeleton className="h-32 w-full" />
             <div className="space-y-2">
-              {[1, 2, 3, 4].map((i) => (
-                <div
-                  key={i}
-                  className="flex items-start space-x-2 rounded-md border p-4"
-                >
-                  <Skeleton className="h-4 w-4" />
-                  <Skeleton className="h-16 w-full" />
-                </div>
+              {[1, 2, 3, 4, 5].map((i) => (
+                <Skeleton key={i} className="h-16 w-full" />
               ))}
             </div>
           </CardContent>
