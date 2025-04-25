1ac2297371cdc35098ec2620bca8326d14ab1772
Leonardo Nunes <leonunesbs@gmail.com>
2025-04-25 19:48:56 -0300

    fix: fix

--- src/app/(withAuth)/(withDashboard)/playlists/[id]/[playlistItemId]/loading.tsx
+++ src/app/(withAuth)/(withDashboard)/playlists/[id]/[playlistItemId]/loading.tsx
@@ -11,48 +11,35 @@ import { Skeleton } from "~/components/ui/skeleton";
 
 export default function PlaylistItemLoading() {
   return (
-    <div>
-      <div className="mb-4 flex items-center justify-between">
-        <Skeleton className="h-8 w-48" />
-        <Skeleton className="h-8 w-24" />
-      </div>
-      <div className="mb-6">
-        <Skeleton className="h-2 w-full" />
-      </div>
-      <div className="space-y-4">
-        <Skeleton className="h-10 w-full" />
-        <Card>
-          <CardHeader>
-            <div className="flex items-start justify-between">
-              <div>
-                <CardTitle>
-                  <Skeleton className="h-6 w-48" />
-                </CardTitle>
-                <CardDescription>
-                  <Skeleton className="mt-2 h-4 w-32" />
-                </CardDescription>
-              </div>
-              <Skeleton className="h-6 w-6" />
-            </div>
-          </CardHeader>
-          <CardContent className="space-y-4">
-            <Skeleton className="h-32 w-full" />
-            <div className="space-y-2">
-              {[1, 2, 3, 4].map((i) => (
-                <Skeleton key={i} className="h-16 w-full" />
-              ))}
-            </div>
-          </CardContent>
-          <CardFooter className="flex flex-col justify-between gap-4 sm:flex-row">
-            <div className="flex flex-col items-center gap-2 sm:flex-row sm:items-start">
-              <Skeleton className="h-10 w-24" />
-              <Skeleton className="h-10 w-24" />
-              <Skeleton className="h-10 w-24" />
-            </div>
-          </CardFooter>
-        </Card>
-        <Skeleton className="h-10 w-full" />
-      </div>
-    </div>
+    <Card>
+      <CardHeader>
+        <div className="flex items-start justify-between">
+          <div>
+            <CardTitle>
+              <Skeleton className="h-6 w-48" />
+            </CardTitle>
+            <CardDescription>
+              <Skeleton className="mt-2 h-4 w-32" />
+            </CardDescription>
+          </div>
+          <Skeleton className="h-6 w-6" />
+        </div>
+      </CardHeader>
+      <CardContent className="space-y-4">
+        <Skeleton className="h-32 w-full" />
+        <div className="space-y-2">
+          {[1, 2, 3, 4].map((i) => (
+            <Skeleton key={i} className="h-16 w-full" />
+          ))}
+        </div>
+      </CardContent>
+      <CardFooter className="flex flex-col justify-between gap-4 sm:flex-row">
+        <div className="flex flex-col items-center gap-2 sm:flex-row sm:items-start">
+          <Skeleton className="h-10 w-24" />
+          <Skeleton className="h-10 w-24" />
+          <Skeleton className="h-10 w-24" />
+        </div>
+      </CardFooter>
+    </Card>
   );
 }
