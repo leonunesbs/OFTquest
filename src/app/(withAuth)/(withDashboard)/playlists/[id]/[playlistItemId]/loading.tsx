189d086af0df2f2afc33122255f0d2f618bbf537
Leonardo Nunes <leonunesbs@gmail.com>
2025-04-25 19:09:25 -0300

    feat: update loading

--- src/app/(withAuth)/(withDashboard)/playlists/[id]/[playlistItemId]/loading.tsx
+++ src/app/(withAuth)/(withDashboard)/playlists/[id]/[playlistItemId]/loading.tsx
@@ -11,26 +11,87 @@ export default function Loading() {
         <Skeleton className="h-2 w-full" />
       </div>
       <div className="space-y-4">
-        <div className="flex justify-between">
-          <Skeleton className="h-10 w-24" />
-          <Skeleton className="h-10 w-24" />
-        </div>
-
-        <div className="space-y-4 rounded-lg border p-4">
-          <Skeleton className="h-6 w-3/4" />
-          <Skeleton className="h-4 w-full" />
-          <Skeleton className="h-4 w-5/6" />
-          <Skeleton className="h-4 w-4/6" />
-          <div className="mt-4 space-y-2">
-            <Skeleton className="h-10 w-full" />
-            <Skeleton className="h-10 w-full" />
-            <Skeleton className="h-10 w-full" />
+        {/* Pagination Top */}
+        <div className="flex items-center justify-center">
+          <div className="flex items-center gap-2">
+            <Skeleton className="h-10 w-10" />
+            <Skeleton className="h-10 w-10" />
+            <div className="flex gap-2">
+              <Skeleton className="h-10 w-10" />
+              <Skeleton className="h-10 w-10" />
+              <Skeleton className="h-10 w-10" />
+              <Skeleton className="h-10 w-10" />
+              <Skeleton className="h-10 w-10" />
+            </div>
+            <Skeleton className="h-10 w-10" />
+            <Skeleton className="h-10 w-10" />
           </div>
         </div>
 
-        <div className="flex justify-between">
-          <Skeleton className="h-10 w-24" />
-          <Skeleton className="h-10 w-24" />
+        {/* Playlist Item Card */}
+        <div className="rounded-lg border">
+          <div className="border-b p-6">
+            <div className="flex items-start justify-between">
+              <div className="space-y-2">
+                <Skeleton className="h-6 w-48" />
+                <Skeleton className="h-4 w-32" />
+              </div>
+              <Skeleton className="h-6 w-6" />
+            </div>
+          </div>
+          <div className="space-y-4 p-6">
+            {/* Question Statement */}
+            <div className="space-y-2">
+              <Skeleton className="h-4 w-full" />
+              <Skeleton className="h-4 w-5/6" />
+              <Skeleton className="h-4 w-4/6" />
+            </div>
+
+            {/* Question Image */}
+            <div className="flex justify-center">
+              <Skeleton className="h-[300px] w-[400px]" />
+            </div>
+
+            {/* Options */}
+            <div className="space-y-4">
+              {[1, 2, 3, 4, 5].map((i) => (
+                <div
+                  key={i}
+                  className="flex items-start space-x-2 rounded-md border p-4"
+                >
+                  <Skeleton className="h-4 w-4" />
+                  <div className="flex-1 space-y-2">
+                    <Skeleton className="h-4 w-full" />
+                    <Skeleton className="h-4 w-5/6" />
+                  </div>
+                </div>
+              ))}
+            </div>
+          </div>
+          <div className="border-t p-6">
+            <div className="flex flex-col items-center gap-4 sm:flex-row sm:items-start">
+              <Skeleton className="h-10 w-32" />
+              <Skeleton className="h-10 w-32" />
+              <Skeleton className="h-10 w-32" />
+            </div>
+          </div>
+        </div>
+
+        {/* Pagination Bottom */}
+        <div className="flex items-center justify-center">
+          <div className="flex items-center gap-2">
+            <Skeleton className="h-10 w-10" />
+            <Skeleton className="h-10 w-10" />
+            <div className="flex gap-2">
+              <Skeleton className="h-10 w-10" />
+              <Skeleton className="h-10 w-10" />
+              <Skeleton className="h-10 w-10" />
+              <Skeleton className="h-10 w-10" />
+              <Skeleton className="h-10 w-10" />
+            </div>
+            <Skeleton className="h-10 w-10" />
+            <Skeleton className="h-10 w-10" />
+          </div>
         </div>
       </div>
     </div>
