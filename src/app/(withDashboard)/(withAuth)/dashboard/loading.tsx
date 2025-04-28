0fc4595c262e9e85d1cc65fc1c3db8ca316fafbf
Leonardo Nunes <leonunesbs@gmail.com>
2025-04-27 22:43:35 -0300

    refactor: simplify loading skeleton structure in DashboardLoading component

--- src/app/(withDashboard)/(withAuth)/dashboard/loading.tsx
+++ src/app/(withDashboard)/(withAuth)/dashboard/loading.tsx
@@ -20,24 +20,15 @@ export default function DashboardLoading() {
       </Card>
 
       <div className="space-y-6">
-        <div className="flex items-center justify-between">
-          <div className="flex gap-2">
-            <Skeleton className="h-10 w-24" />
-            <Skeleton className="h-10 w-24" />
-            <Skeleton className="h-10 w-24" />
-            <Skeleton className="h-10 w-24" />
-          </div>
-        </div>
-
         <div className="grid gap-6 md:grid-cols-2">
           <Card>
             <CardHeader>
               <Skeleton className="h-6 w-32" />
               <Skeleton className="h-4 w-48" />
             </CardHeader>
-            <CardHeader>
-              <Skeleton className="h-8 w-16" />
-            </CardHeader>
+            <div className="p-6">
+              <Skeleton className="h-64 w-full" />
+            </div>
           </Card>
 
           <Card>
@@ -45,9 +36,9 @@ export default function DashboardLoading() {
               <Skeleton className="h-6 w-32" />
               <Skeleton className="h-4 w-48" />
             </CardHeader>
-            <CardHeader>
-              <Skeleton className="h-8 w-16" />
-            </CardHeader>
+            <div className="p-6">
+              <Skeleton className="h-64 w-full" />
+            </div>
           </Card>
         </div>
 
@@ -56,22 +47,10 @@ export default function DashboardLoading() {
             <Skeleton className="h-6 w-32" />
             <Skeleton className="h-4 w-48" />
           </CardHeader>
-          <CardHeader>
+          <div className="p-6">
             <Skeleton className="h-64 w-full" />
-          </CardHeader>
+          </div>
         </Card>
-
-        <div className="grid gap-6 md:grid-cols-2">
-          <Card>
-            <CardHeader>
-              <Skeleton className="h-6 w-32" />
-              <Skeleton className="h-4 w-48" />
-            </CardHeader>
-            <CardHeader>
-              <Skeleton className="h-64 w-full" />
-            </CardHeader>
-          </Card>
-        </div>
       </div>
     </div>
   );
